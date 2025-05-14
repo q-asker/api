a7821dd79e487aea02b6b9ea7a0d824e8ea9e5a5
Oh YoungJe <pine7420@naver.com>
2025-05-14 17:42:51 +0900

    initial commit

---
+++ src/main/java/com/slb/qasker/QAskerApplication.java
@@ -0,0 +1,13 @@
+package com.slb.qasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QAskerApplication.class, args);
+    }
+
+}
