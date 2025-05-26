ec8d2da5c44ee02dd877a8550467ab60bc434f51
lhoju0158 <lhoju0158@gmail.com>
2025-05-26 22:05:06 +0900

    [ICC-23] Error 처리 코드 추가

--- src/main/java/com/icc/qasker/global/error/ExceptionMessage.java
+++ src/main/java/com/icc/qasker/global/error/ExceptionMessage.java
@@ -17,7 +17,30 @@ public enum ExceptionMessage {
     // ExplanationService.java
     PROBLEM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 문제를 찾을 수 없습니다."),
     NULL_ANSWER_INPUT(HttpStatus.BAD_REQUEST, "해당 문제에 대한 정답이 없습니다."),
-    INVALID_CORRECT_ANSWER(HttpStatus.INTERNAL_SERVER_ERROR, "사용자 정답이 존재하지 않습니다.");
+    INVALID_CORRECT_ANSWER(HttpStatus.INTERNAL_SERVER_ERROR, "사용자 정답이 존재하지 않습니다."),
+
+    // GenerationService.java
+    // AI ISSUE
+    AI_SERVER_TIMEOUT(HttpStatus.REQUEST_TIMEOUT,"AI서버 응답 시간이 초과되었습니다."),
+    AI_SERVER_CONNECTION_FAILED(HttpStatus.BAD_GATEWAY, "AI서버에 연결할 수 없습니다."),
+    AI_SERVER_RESPONSE_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "AI서버에서 오류가 발생했습니다."),
+
+    NULL_AI_RESPONSE(HttpStatus.BAD_REQUEST,"AI 응답이 null입니다."),
+    EMPTY_QUIZ_LIST(HttpStatus.BAD_REQUEST,"퀴즈 목록이 비어있습니다."),
+    NULL_QUIZ(HttpStatus.BAD_REQUEST,"퀴즈가 null입니다."),
+    INVALID_QUIZ_TITLE(HttpStatus.BAD_REQUEST,"유효하지 않은 지문입니다."),
+    INVALID_SELECTIONS(HttpStatus.BAD_REQUEST,"유효하지 않은 선택지들입니다."),
+    INVALID_CORRECT_ANSWER_INDEX(HttpStatus.BAD_REQUEST,"유효하지 않은 정답입니다."),
+    INVALID_EXPLANATION(HttpStatus.BAD_REQUEST,"유효하지 않은 해설입니다."),
+
+    // FE ISSUE
+    NULL_GENERATION_REQUEST(HttpStatus.BAD_REQUEST, "생성 요청이 null입니다."),
+    INVALID_URL(HttpStatus.BAD_REQUEST,"URL이 없습니다."),
+    INVALID_QUESTION_COUNT(HttpStatus.BAD_REQUEST,"문제 개수가 유효하지 않습니다."),
+    INVALID_TYPE(HttpStatus.BAD_REQUEST,"난이도가 유효하지 않습니다."),
+
+    // Default
+    DEFAULT_ERROR(HttpStatus.BAD_REQUEST,"에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
