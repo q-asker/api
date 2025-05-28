d8d69048cbe0784ee0084c283a4f4c7806eff1be
Oh YoungJe <pine7420@naver.com>
2025-05-28 16:43:56 +0900

    [ICC-37] 구현 완료

--- src/main/java/com/icc/qasker/quiz/repository/ProblemSetRepository.java
+++ src/main/java/com/icc/qasker/quiz/repository/ProblemSetRepository.java
@@ -3,5 +3,7 @@ package com.icc.qasker.quiz.repository;
 import com.icc.qasker.quiz.entity.ProblemSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProblemSetRepository extends JpaRepository<ProblemSet,Long> {
+public interface ProblemSetRepository extends JpaRepository<ProblemSet, Long> {
+
+    ProblemSet getProblemSetById(Long id);
 }
