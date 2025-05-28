9a2b8f5312e8f49e5834a6534dbe72f69482cfae
Oh YoungJe <pine7420@naver.com>
2025-05-28 22:56:27 +0900

    [ICC-43] Swagger 설정

---
+++ src/main/java/com/icc/qasker/quiz/controller/doc/ProblemSetApiDoc.java
@@ -0,0 +1,16 @@
+package com.icc.qasker.quiz.controller.doc;
+
+import com.icc.qasker.quiz.dto.response.ProblemSetResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Tag(name = "ProblemSet", description = "문제세트 관련 API")
+public interface ProblemSetApiDoc {
+
+    @Operation(summary = "문제세트를 가져온다")
+    @GetMapping("/{id}")
+    ResponseEntity<ProblemSetResponse> getProblemSet(@PathVariable("id") Long problemSetId);
+}
