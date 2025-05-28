a0fadab5895ad77c7bee56c78c0e95561d700c3e
Oh YoungJe <pine7420@naver.com>
2025-05-28 22:47:49 +0900

    [ICC-43] 기본적인 설정 완료

--- src/main/java/com/icc/qasker/quiz/controller/ExplanationController.java
+++ src/main/java/com/icc/qasker/quiz/controller/ExplanationController.java
@@ -1,23 +1,30 @@
 package com.icc.qasker.quiz.controller;
 
+import com.icc.qasker.quiz.controller.doc.ExplanationApiDoc;
 import com.icc.qasker.quiz.dto.response.ExplanationResponse;
 import com.icc.qasker.quiz.dto.response.ResultResponse;
 import com.icc.qasker.quiz.service.ExplanationService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/explanation")
-public class ExplanationController {
+public class ExplanationController implements ExplanationApiDoc {
+
     private final ExplanationService explanationService;
 
+    @Override
     @GetMapping("/{id}")
-    public ResponseEntity<ExplanationResponse> getExplanation(@PathVariable("id") Long problemSetId) {
-        List<ResultResponse> results = explanationService.getExplanationByProblemSetId(problemSetId);
+    public ResponseEntity<ExplanationResponse> getExplanation(
+        @PathVariable("id") Long problemSetId) {
+        List<ResultResponse> results = explanationService.getExplanationByProblemSetId(
+            problemSetId);
         return ResponseEntity.ok(new ExplanationResponse(results));
     }
 }
