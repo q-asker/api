1b2910db7a8ce93a6f5ae4daa54736a89cfb9abe
lhoju0158 <lhoju0158@gmail.com>
2025-06-08 19:12:03 +0900

    [ICC-73] selection content 수정

--- src/main/java/com/icc/qasker/quiz/entity/Selection.java
+++ src/main/java/com/icc/qasker/quiz/entity/Selection.java
@@ -1,21 +1,38 @@
 package com.icc.qasker.quiz.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Getter @NoArgsConstructor @Setter @AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Setter
+@AllArgsConstructor
 public class Selection {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(columnDefinition = "TEXT")
     private String content;
     @Column(nullable = false)
     private boolean correct;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumns({
-            @JoinColumn(name = "problem_set_id", referencedColumnName = "problem_set_id"),
-            @JoinColumn(name = "number", referencedColumnName = "number")
+        @JoinColumn(name = "problem_set_id", referencedColumnName = "problem_set_id"),
+        @JoinColumn(name = "number", referencedColumnName = "number")
     })
     private Problem problem;
 }
