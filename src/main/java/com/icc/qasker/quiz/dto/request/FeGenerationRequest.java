afd99d461d7540cc2938b4277e1c802dd61b96ca
lhoju0158 <lhoju0158@gmail.com>
2025-06-08 14:54:15 +0900

    [ICC-71] page 응답형식 수정

--- src/main/java/com/icc/qasker/quiz/dto/request/FeGenerationRequest.java
+++ src/main/java/com/icc/qasker/quiz/dto/request/FeGenerationRequest.java
@@ -1,12 +1,12 @@
 package com.icc.qasker.quiz.dto.request;
 
+import com.icc.qasker.global.error.CustomException;
+import com.icc.qasker.global.error.ExceptionMessage;
 import com.icc.qasker.quiz.domain.enums.DifficultyType;
 import com.icc.qasker.quiz.domain.enums.QuizType;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import java.util.List;
 import lombok.Getter;
 
 @Getter
@@ -22,12 +22,22 @@ public class FeGenerationRequest {
     private DifficultyType difficultyType;
     @NotNull(message = "pageSelected가 null입니다.")
     private boolean pageSelected;
-    @NotEmpty(message = "selectedPages가 비어있습니다.")
-    private List<@Min(1) Integer> selectedPages;
+    @NotNull(message = "startPage가 null입니다.")
+    @Min(1)
+    private int startPage;
+    @NotNull(message = "endPage가 null입니다.")
+    @Min(1)
+    private int endPage;
 
     public void validateQuizCount() {
         if (quizCount % 5 != 0) {
-            throw new IllegalArgumentException("quizCount는 5배수입니다.");
+            throw new CustomException(ExceptionMessage.INVALID_QUIZ_COUNT_REQUEST);
+        }
+    }
+
+    public void validatePageSize() {
+        if ((endPage - startPage + 1) > 100) {
+            throw new CustomException(ExceptionMessage.INVALID_QUIZ_NUMBER_REQUEST);
         }
     }
 }
