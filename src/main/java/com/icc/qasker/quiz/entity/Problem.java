d75bebd73311383cb46a0d1459d21e210cdf11fe
lhoju0158 <lhoju0158@gmail.com>
2025-06-08 19:10:09 +0900

    [ICC-73] problem title 수정

--- src/main/java/com/icc/qasker/quiz/entity/Problem.java
+++ src/main/java/com/icc/qasker/quiz/entity/Problem.java
@@ -1,16 +1,32 @@
 package com.icc.qasker.quiz.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Getter @AllArgsConstructor @Setter @NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Setter
+@NoArgsConstructor
 public class Problem {
+
     @EmbeddedId
     private ProblemId id;
+    @Column(columnDefinition = "TEXT")
     private String title;
 
     @ManyToOne(fetch = FetchType.LAZY)
