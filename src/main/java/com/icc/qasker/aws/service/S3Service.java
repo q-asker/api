e822465f7698c68d910b9fdfaf90834a80b414a4
Oh YoungJe <pine7420@naver.com>
2025-06-16 02:32:26 +0900

    [ICC-97] 자바 이미지 + 변환기 구현 완료

--- src/main/java/com/icc/qasker/aws/service/S3Service.java
+++ src/main/java/com/icc/qasker/aws/service/S3Service.java
@@ -3,15 +3,18 @@ package com.icc.qasker.aws.service;
 import com.icc.qasker.aws.dto.S3UploadRequest;
 import com.icc.qasker.aws.dto.S3UploadResponse;
 import com.icc.qasker.aws.util.FileUploadValidator;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import com.icc.qasker.aws.util.PPTtoPDFConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class S3Service {
@@ -21,12 +24,14 @@ public class S3Service {
     @Value("${aws.cloudfront.base-url}")
     private String cloudFrontBaseUrl;
 
+    private final PPTtoPDFConverter ppTtoPDFConverter;
     private final FileUploadValidator fileUploadValidator;
     private final S3Client s3Client;
 
-    public S3Service(FileUploadValidator fileUploadValidator, S3Client s3Client) {
+    public S3Service(FileUploadValidator fileUploadValidator, S3Client s3Client, PPTtoPDFConverter ppTtoPDFConverter) {
         this.fileUploadValidator = fileUploadValidator;
         this.s3Client = s3Client;
+        this.ppTtoPDFConverter = ppTtoPDFConverter;
     }
 
     public S3UploadResponse uploadFile(S3UploadRequest s3UploadRequest) {
@@ -34,24 +39,39 @@ public class S3Service {
         fileUploadValidator.checkOf(multipartFile);
 
         String timestamp = LocalDateTime.now()
-            .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmssSSS"));
+                .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmssSSS"));
         String keyName = timestamp + "_" + multipartFile.getOriginalFilename();
+        if (keyName.endsWith(".pdf")) {
+            PutObjectRequest putObjectRequest =
+                    PutObjectRequest
+                            .builder()
+                            .bucket(bucketName)
+                            .key(keyName)
+                            .build();
 
-        PutObjectRequest putObjectRequest =
-            PutObjectRequest
-                .builder()
-                .bucket(bucketName)
-                .key(keyName)
-                .build();
-
+            try {
+                s3Client.putObject(putObjectRequest,
+                        RequestBody.fromInputStream(multipartFile.getInputStream(),
+                                multipartFile.getSize()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return S3UploadResponse.builder().uploadedUrl(cloudFrontBaseUrl + "/" + keyName).build();
+        }
         try {
+            File convertedFile = ppTtoPDFConverter.convertPPTtoPDF(multipartFile);
+            String pdfKyName = keyName.replace(".pptx", ".pdf");
+            PutObjectRequest putObjectRequest =
+                    PutObjectRequest
+                            .builder()
+                            .bucket(bucketName)
+                            .key(pdfKyName)
+                            .build();
             s3Client.putObject(putObjectRequest,
-                RequestBody.fromInputStream(multipartFile.getInputStream(),
-                    multipartFile.getSize()));
+                    RequestBody.fromFile(convertedFile));
+            return S3UploadResponse.builder().uploadedUrl(cloudFrontBaseUrl + "/" + pdfKyName).build();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        return S3UploadResponse.builder().uploadedUrl(cloudFrontBaseUrl + "/" + keyName).build();
     }
 }
