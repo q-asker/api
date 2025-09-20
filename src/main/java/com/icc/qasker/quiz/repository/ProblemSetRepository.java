012fe4ec4ce6efb9dcc90fd6a2edba8437db2c78
Oh YoungJe <pine7420@naver.com>
2025-09-20 20:37:35 +0900

    [ICC-145] 레포지토리에도 메서드 추가

--- src/main/java/com/icc/qasker/quiz/repository/ProblemSetRepository.java
+++ src/main/java/com/icc/qasker/quiz/repository/ProblemSetRepository.java
@@ -1,10 +1,12 @@
 package com.icc.qasker.quiz.repository;
 
 import com.icc.qasker.quiz.entity.ProblemSet;
+import com.newrelic.api.agent.Trace;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProblemSetRepository extends JpaRepository<ProblemSet, Long> {
 
+    @Trace
     Optional<ProblemSet> getProblemSetById(Long id);
 }
--- src/main/java/com/icc/qasker/quiz/repository/SelectionRepository.java
+++ src/main/java/com/icc/qasker/quiz/repository/SelectionRepository.java
@@ -2,10 +2,12 @@ package com.icc.qasker.quiz.repository;
 
 import com.icc.qasker.quiz.entity.Problem;
 import com.icc.qasker.quiz.entity.Selection;
+import com.newrelic.api.agent.Trace;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SelectionRepository extends JpaRepository<Selection, Long> {
 
+    @Trace
     List<Selection> findByProblem(Problem problem);
 }
