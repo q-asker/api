72973f0e6c3512e1ffab59ba75a0cd900d72a91a
Oh YoungJe <pine7420@naver.com>
2025-10-01 20:10:36 +0900

    [ICC-152] 레디스 설정 책임 분리

--- src/main/java/com/icc/qasker/auth/config/RedisConfig.java
+++ src/main/java/com/icc/qasker/auth/config/RedisConfig.java
@@ -1,35 +1,23 @@
 package com.icc.qasker.auth.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+    private final RedisProperties redisProperties;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Value("${spring.data.redis.password}")
-    private String password;
-
-    @Value("${spring.data.redis.database}")
-    private int database;
+    @Autowired
+    public RedisConfig(RedisProperties redisProperties) {
+        this.redisProperties = redisProperties;
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(host);
-        config.setPort(port);
-        config.setPassword(password);
-        config.setDatabase(database);
-
-        return new LettuceConnectionFactory(config);
+        return new LettuceConnectionFactory(redisProperties.getRedisConfiguration());
     }
 }
