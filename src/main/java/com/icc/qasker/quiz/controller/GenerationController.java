98b66f53ac305ada54a1892a16fc1988bf81d4cc
Oh YoungJe <pine7420@naver.com>
2025-10-21 02:22:54 +0900

    [ICC-170] 아마존 웹 서비스 오류 대응

--- src/main/java/com/icc/qasker/quiz/controller/GenerationController.java
+++ src/main/java/com/icc/qasker/quiz/controller/GenerationController.java
@@ -1,11 +1,14 @@
 package com.icc.qasker.quiz.controller;
 
+import com.icc.qasker.global.error.CustomException;
+import com.icc.qasker.global.error.ExceptionMessage;
 import com.icc.qasker.quiz.controller.doc.GenerationApiDoc;
 import com.icc.qasker.quiz.dto.request.FeGenerationRequest;
 import com.icc.qasker.quiz.dto.response.GenerationResponse;
 import com.icc.qasker.quiz.service.GenerationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,19 @@ import reactor.core.publisher.Mono;
 public class GenerationController implements GenerationApiDoc {
 
     private final GenerationService generationService;
+    private boolean awsError = false;
+
+    @GetMapping("/toggle-aws-error")
+    public void setAwsError() {
+        awsError = !awsError;
+    }
 
     @PostMapping
     public Mono<GenerationResponse> postProblemSetId(
         @Valid @RequestBody FeGenerationRequest feGenerationRequest) {
+        if (awsError) {
+            throw new CustomException(ExceptionMessage.AWS_SERVICE_ERROR);
+        }
         return generationService.processGenerationRequest(feGenerationRequest);
     }
-
 }
