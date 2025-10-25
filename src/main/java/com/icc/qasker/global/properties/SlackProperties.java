2d0066846ed8d107dea5b112c245040e6e5578c3
Oh YoungJe <pine7420@naver.com>
2025-10-25 21:41:14 +0900

    [ICC-178] 클래스에 책임을 넘김

--- src/main/java/com/icc/qasker/global/properties/SlackProperties.java
+++ src/main/java/com/icc/qasker/global/properties/SlackProperties.java
@@ -12,6 +12,6 @@ public class SlackProperties {
 
     private final boolean enabled;
     private final URI webhookUrlNotify;
-    private final String usernameNotify;
+    private final String usernameNotify = "퀴즈생성 알림이";
     private final String iconNotify = ":white_check_mark:";
 }
