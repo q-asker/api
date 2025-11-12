f2b6c6998916cd30ee6b3b97e117e9d5ad0777ec
lhoju0158 <lhoju0158@gmail.com>
2025-11-13 01:18:33 +0900

    [ICC-181] refactor: WebConfig timeout 설정 추가

--- modules/global/src/main/java/com/icc/qasker/global/component/WebConfig.java
+++ modules/global/src/main/java/com/icc/qasker/global/component/WebConfig.java
@@ -1,10 +1,12 @@
 package com.icc.qasker.global.component;
 
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -35,16 +37,26 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean("aiRestClient")
     public RestClient aiRestClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(10));
+        factory.setReadTimeout(Duration.ofMinutes(2));
+
         return RestClient.builder()
             .baseUrl(aiServerUrl)
+            .requestFactory(factory)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build();
     }
 
     @Bean("aiMockingRestClient")
     public RestClient aiMockingRestClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(10));
+        factory.setReadTimeout(Duration.ofMinutes(2));
+        
         return RestClient.builder()
             .baseUrl(aiMockingServerUrl)
+            .requestFactory(factory)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build();
     }
