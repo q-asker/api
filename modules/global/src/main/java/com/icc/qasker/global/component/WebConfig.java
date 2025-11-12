77991cdf4ba1418309dcee3f62954d764141469a
lhoju0158 <lhoju0158@gmail.com>
2025-11-13 00:56:12 +0900

    [ICC-181] refactor: aiClient 변경 webClient -> restClient

--- modules/global/src/main/java/com/icc/qasker/global/component/WebConfig.java
+++ modules/global/src/main/java/com/icc/qasker/global/component/WebConfig.java
@@ -1,7 +1,11 @@
 package com.icc.qasker.global.component;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,11 +18,34 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${q-asker.frontend-dev-url}")
     private String frontEndDevUrl;
 
+    @Value("${q-asker.ai-server-url}")
+    private String aiServerUrl;
+
+    @Value("${q-asker.ai-mocking-server-url}")
+    private String aiMockingServerUrl;
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins(frontEndDeployUrl, frontEndDevUrl)
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .maxAge(3600);
+    }
+
+    @Bean("aiRestClient")
+    public RestClient aiRestClient() {
+        return RestClient.builder()
+            .baseUrl(aiServerUrl)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+
+    @Bean("aiMockingRestClient")
+    public RestClient aiMockingRestClient() {
+        return RestClient.builder()
+            .baseUrl(aiMockingServerUrl)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
     }
 }
