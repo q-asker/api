d494e35a1d65e6907f0fb524bbb2d7a5e9801826
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-01-18 15:43:40 +0900

    [ICC-202] 모킹 어댑터

--- modules/quiz/api/src/main/java/com/icc/qasker/quiz/dto/response/AiGenerationResponse.java
+++ modules/quiz/api/src/main/java/com/icc/qasker/quiz/dto/response/AiGenerationResponse.java
@@ -3,9 +3,13 @@ package com.icc.qasker.quiz.dto.response;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AiGenerationResponse {
 
     private String title;
