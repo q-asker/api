7cd24adb5883da3d1b6a8f7104867837c1b45cd3
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-02-09 23:19:19 +0900

    [ICC-249] 사용하지 않는 dto 제거

--- modules/aws/api/src/main/java/com/icc/qasker/aws/S3ValidateService.java
+++ modules/aws/api/src/main/java/com/icc/qasker/aws/S3ValidateService.java
@@ -2,8 +2,5 @@ package com.icc.qasker.aws;
 
 public interface S3ValidateService {
 
-    void checkCloudFrontUrlWithThrowing(String url);
-
-
     void validateFileWithThrowing(String fileName, long fileSize, String contentType);
 }
