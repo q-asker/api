87c5603da7723a934c743527b683ef27f42abe59
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-02-09 23:09:06 +0900

    [ICC-249] 쓰지 않는 인터페이스 제거

--- modules/quiz/api/src/main/java/com/icc/qasker/quiz/SpecificExplanationService.java
+++
@@ -1,9 +0,0 @@
-package com.icc.qasker.quiz;
-
-import com.icc.qasker.quiz.dto.feresponse.SpecificExplanationResponse;
-
-public interface SpecificExplanationService {
-
-    SpecificExplanationResponse getSpecificExplanation(String encodedProblemSetId, int number);
-}
-
