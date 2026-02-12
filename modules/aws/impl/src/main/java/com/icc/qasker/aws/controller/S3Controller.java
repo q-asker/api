bceb032563dc91290a1533c261d9900784fa90be
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-02-12 16:48:06 +0900

    [ICC-256]  스웨거 인터페이스 제거

--- modules/aws/impl/src/main/java/com/icc/qasker/aws/controller/S3Controller.java
+++ modules/aws/impl/src/main/java/com/icc/qasker/aws/controller/S3Controller.java
@@ -1,10 +1,11 @@
 package com.icc.qasker.aws.controller;
 
 import com.icc.qasker.aws.S3Service;
-import com.icc.qasker.aws.doc.S3ApiDoc;
 import com.icc.qasker.aws.dto.FileExistStatusResponse;
 import com.icc.qasker.aws.dto.PresignRequest;
 import com.icc.qasker.aws.dto.PresignResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "S3", description = "S3 관련 API")
 @RestController
 @RequestMapping("/s3")
 @AllArgsConstructor
-public class S3Controller implements S3ApiDoc {
+public class S3Controller {
 
     public final S3Service s3Service;
 
+    @Operation(summary = "파일이 존재하는지 확인한다")
     @GetMapping("/check-file-exist")
     public ResponseEntity<FileExistStatusResponse> checkFileExist(
         @RequestParam("url") String url
@@ -29,6 +32,7 @@ public class S3Controller implements S3ApiDoc {
         return ResponseEntity.ok(s3Service.checkFileExistence(url));
     }
 
+    @Operation(summary = "프리사인드 URL을 얻는다")
     @PostMapping("/request-presign")
     public ResponseEntity<PresignResponse> upload(
         @Valid
