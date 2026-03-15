85e25ab4149b196f62d50f31f1d1e1f693e7e74f
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-03-15 14:57:08 +0900

    임시

--- modules/global/src/main/java/com/icc/qasker/global/error/GlobalExceptionHandler.java
+++ modules/global/src/main/java/com/icc/qasker/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.icc.qasker.global.error;
 
+import com.icc.qasker.global.component.SlackNotifier;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.ClientAbortException;
 import org.springframework.http.HttpStatus;
@@ -13,7 +17,10 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+  private final SlackNotifier slackNotifier;
 
   @ExceptionHandler(NoResourceFoundException.class)
   public ResponseEntity<Void> handleNoResourceFoundException(NoResourceFoundException e) {
@@ -24,6 +31,7 @@ public class GlobalExceptionHandler {
   public ResponseEntity<CustomErrorResponse> handleCustomException(
       CustomException customException) {
     log.error("Custom Error Occurred", customException);
+    notifyError("Custom Error Occurred", customException);
 
     return ResponseEntity.status(customException.getHttpStatus())
         .body(new CustomErrorResponse(customException.getMessage()));
@@ -39,6 +47,7 @@ public class GlobalExceptionHandler {
   public ResponseEntity<CustomErrorResponse> handleCustomException(
       CallNotPermittedException exception) {
     log.error("Circuit Breaker Activated", exception);
+    notifyError("Circuit Breaker Activated", exception);
 
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(new CustomErrorResponse(ExceptionMessage.AI_SERVER_COMMUNICATION_ERROR.getMessage()));
@@ -57,8 +66,24 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<CustomErrorResponse> handleCustomException(Exception exception) {
     log.error("Unexpected Error Occurred", exception);
+    notifyError("Unexpected Error Occurred", exception);
 
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(new CustomErrorResponse(ExceptionMessage.DEFAULT_ERROR.getMessage()));
+  }
+
+  private void notifyError(String message, Exception exception) {
+    StringWriter sw = new StringWriter();
+    exception.printStackTrace(new PrintWriter(sw));
+    String stackTrace = sw.toString();
+    // 스택트레이스가 너무 길면 잘라냄
+    if (stackTrace.length() > 1500) {
+      stackTrace = stackTrace.substring(0, 1500) + "\n...truncated";
+    }
+    String text =
+        String.format(
+            "*%s*\n```%s: %s```\n```%s```",
+            message, exception.getClass().getSimpleName(), exception.getMessage(), stackTrace);
+    slackNotifier.asyncNotifyError(text);
   }
 }
--- modules/global/src/main/java/com/icc/qasker/global/properties/SlackProperties.java
+++ modules/global/src/main/java/com/icc/qasker/global/properties/SlackProperties.java
@@ -11,11 +11,17 @@ public class SlackProperties {
   private final String webhookUrlNotify;
   private final String usernameNotify;
   private final String iconNotify;
+  private final String webhookUrlError;
+  private final String usernameError;
+  private final String iconError;
 
-  public SlackProperties(boolean enabled, String webhookUrlNotify) {
+  public SlackProperties(boolean enabled, String webhookUrlNotify, String webhookUrlError) {
     this.enabled = enabled;
     this.webhookUrlNotify = webhookUrlNotify;
     this.usernameNotify = "퀴즈생성 알림이";
     this.iconNotify = ":white_check_mark:";
+    this.webhookUrlError = webhookUrlError;
+    this.usernameError = "에러 알림이";
+    this.iconError = ":rotating_light:";
   }
 }
