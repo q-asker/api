2d1b3b437364989aad901f7e4fea5bc92035f5bf
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-03-18 15:08:58 +0900

    [ICC-282] ✨ feat: Auth/Util 컨트롤러에 Swagger 어노테이션 추가
    AuthController, HelloController에 @Tag, @Operation 어노테이션을 추가하고
    auth-impl에 springdoc-openapi 의존성 추가.

--- modules/util/impl/src/main/java/com/icc/qasker/util/controller/HelloController.java
+++ modules/util/impl/src/main/java/com/icc/qasker/util/controller/HelloController.java
@@ -1,17 +1,21 @@
 package com.icc.qasker.util.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Health", description = "서버 상태 확인 API")
 @RestController
 public class HelloController {
 
   @Value("${spring.profiles.active:default}")
   private String activeProfile;
 
+  @Operation(summary = "서버 상태 및 활성 프로필을 확인한다")
   @GetMapping("/status")
   public Map<String, Object> status() {
     Map<String, Object> response = new HashMap<>();
