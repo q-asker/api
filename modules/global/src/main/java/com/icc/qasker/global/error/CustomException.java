a4aca53b9ada3a96836fa3ac809f7a2217e2ca21
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-03-18 15:08:51 +0900

    [ICC-282] ♻️ refactor: CustomException에 context/cause 전달 지원 및 분산 로깅 제거
    CustomException에 context, cause를 받는 생성자를 추가하고
    GlobalExceptionHandler에서 context를 포함한 구조화 로깅을 수행하도록 개선.
    각 서비스의 log.error() + throw 패턴을 throw new CustomException(msg, context, cause)로 통합.

--- modules/global/src/main/java/com/icc/qasker/global/error/CustomException.java
+++ modules/global/src/main/java/com/icc/qasker/global/error/CustomException.java
@@ -8,16 +8,40 @@ public class CustomException extends RuntimeException {
 
   private final HttpStatus httpStatus;
   private final String message;
+  private final String context;
 
   public CustomException(ExceptionMessage exceptionMessage) {
     super(exceptionMessage.getMessage());
     this.httpStatus = exceptionMessage.getHttpStatus();
     this.message = exceptionMessage.getMessage();
+    this.context = null;
   }
 
   public CustomException(HttpStatus httpStatus, String message) {
     super(message);
     this.httpStatus = httpStatus;
     this.message = message;
+    this.context = null;
+  }
+
+  public CustomException(ExceptionMessage exceptionMessage, String context) {
+    super(exceptionMessage.getMessage());
+    this.httpStatus = exceptionMessage.getHttpStatus();
+    this.message = exceptionMessage.getMessage();
+    this.context = context;
+  }
+
+  public CustomException(ExceptionMessage exceptionMessage, Throwable cause) {
+    super(exceptionMessage.getMessage(), cause);
+    this.httpStatus = exceptionMessage.getHttpStatus();
+    this.message = exceptionMessage.getMessage();
+    this.context = null;
+  }
+
+  public CustomException(ExceptionMessage exceptionMessage, String context, Throwable cause) {
+    super(exceptionMessage.getMessage(), cause);
+    this.httpStatus = exceptionMessage.getHttpStatus();
+    this.message = exceptionMessage.getMessage();
+    this.context = context;
   }
 }
