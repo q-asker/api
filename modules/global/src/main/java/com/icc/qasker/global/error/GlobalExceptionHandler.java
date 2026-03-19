9f21315597165a4ce7c70d1f16784610aa79806b
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2026-03-19 22:02:37 +0900

    [ICC-291] 🔊 fix: GlobalExceptionHandler 누락된 에러 로그 추가

--- modules/global/src/main/java/com/icc/qasker/global/error/GlobalExceptionHandler.java
+++ modules/global/src/main/java/com/icc/qasker/global/error/GlobalExceptionHandler.java
@@ -36,6 +36,7 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(ClientSideException.class)
   public ResponseEntity<CustomErrorResponse> handleClientException(ClientSideException e) {
+    log.warn("클라이언트 요청 오류: {}", e.getMessage());
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(new CustomErrorResponse(e.getMessage()));
   }
@@ -52,6 +53,7 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(MaxUploadSizeExceededException.class)
   public ResponseEntity<CustomErrorResponse> handleMaxUploadSizeExceededException(
       MaxUploadSizeExceededException e) {
+    log.warn("파일 업로드 크기 초과: {}", e.getMessage());
     return ResponseEntity.status(ExceptionMessage.FILE_SIZE_EXCEEDED.getHttpStatus())
         .body(new CustomErrorResponse(ExceptionMessage.FILE_SIZE_EXCEEDED.getMessage()));
   }
